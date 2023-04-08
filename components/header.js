b745e04c89e283df440a1a3364de0149b9fb62f3
Kyle <kyle.jon.carlson@gmail.com>
2023-04-07 22:13:01 -0700

    Added page for RMR project
    The logo of Robots Moons Raccoons leads to its own page now. The header is now its own component stored in a separate js file.

---
+++ components/header.js
@@ -0,0 +1,39 @@
+class Header extends HTMLElement {
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        this.innerHTML = `
+            <header>
+                <h1>
+                    KYLE
+                </h1>
+            </header>
+
+            <style>
+                header {
+                    position: absolute;
+                    top: 10px;
+                    left: 0;
+                    right: 0;
+                    background-image: url("assets/cityside_night.png");
+                    background-repeat: no-repeat;
+                    background-size: cover;
+                    background-attachment: fixed;
+                    text-align: left;
+                    padding-left: 30px;
+                    padding-top: 0px;
+                    padding-bottom: 0px;
+                }
+                header h1{
+                    font-size:5em;
+                    text-align: left;
+                    color:whitesmoke;
+                }
+            </style>
+        `;
+    }
+}
+
+customElements.define('header-component', Header);
